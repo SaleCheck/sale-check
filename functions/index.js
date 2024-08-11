b0a5a8e97051da8c2f454139997e2a5ced891e48
MathiasSchindlerCPH <mathingvid@gmail.com>
2024-08-11 15:11:22 +0200

    remove test functinos

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
-const testPavoniPriceMultiple = require('./productPrices/testPavoniPriceMultiple');
-const testPavoniPrice = require('./productPrices/testPavoniPrice');
+//const testPavoniPriceMultiple = require('./productPrices/testPavoniPriceMultiple');
+//const testPavoniPrice = require('./productPrices/testPavoniPrice');
 const scrapeAndComparePrices = require('./productPrices/scrapeAndComparePrices');
 
-exports.testPavoniPriceMultiple = testPavoniPriceMultiple.testPavoniPriceMultiple;
-exports.testPavoniPrice = testPavoniPrice.testPavoniPrice;
+//exports.testPavoniPriceMultiple = testPavoniPriceMultiple.testPavoniPriceMultiple;
+//exports.testPavoniPrice = testPavoniPrice.testPavoniPrice;
 exports.scrapeAndComparePrices = scrapeAndComparePrices.scrapeAndComparePrices;
