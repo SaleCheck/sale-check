04ad228a5fd709c09712c7e6fbf717287edad949
MathiasSchindlerCPH <mathingvid@gmail.com>
2024-08-12 19:35:31 +0200

    add new renamed files and initializeApp()

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
+const { initializeApp } = require("firebase-admin/app");
+initializeApp();
+
 //const testPavoniPriceMultiple = require('./productPrices/testPavoniPriceMultiple');
 //const testPavoniPrice = require('./productPrices/testPavoniPrice');
-const scrapeAndComparePrices = require('./productPrices/scrapeAndComparePrices');
-const scheduleScrapeAndComparePrices = require('./productPrices/scheduleScrapeAndComparePrices');
+const scrapeAndComparePricesOnRequest = require('./productPrices/scrapeAndComparePricesOnRequest');
+const scrapeAndComparePricesOnSchedule = require('./productPrices/scrapeAndComparePricesOnSchedule');
 const onProductSaleCheckExecution = require('./productPrices/onProductSaleCheckExecution');
 
 //exports.testPavoniPriceMultiple = testPavoniPriceMultiple.testPavoniPriceMultiple;
 //exports.testPavoniPrice = testPavoniPrice.testPavoniPrice;
-exports.scrapeAndComparePrices = scrapeAndComparePrices.scrapeAndComparePrices;
-exports.scheduleScrapeAndComparePrices = scheduleScrapeAndComparePrices.scheduleScrapeAndComparePrices;
+exports.scrapeAndComparePricesOnRequest = scrapeAndComparePricesOnRequest.scrapeAndComparePricesOnRequest;
+exports.scrapeAndComparePricesOnSchedule = scrapeAndComparePricesOnSchedule.scrapeAndComparePricesOnSchedule;
 exports.onProductSaleCheckExecution = onProductSaleCheckExecution.onProductSaleCheckExecution;
