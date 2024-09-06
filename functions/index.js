21e5a9e945f13399f386b1e52bdd87f95f4de9d4
MathiasSchindlerCPH <mathingvid@gmail.com>
2024-09-06 18:46:27 +0200

    remove commented test functions

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,10 @@
 const { initializeApp } = require("firebase-admin/app");
 initializeApp();
 
-//const testPavoniPriceMultiple = require('./productPrices/testPavoniPriceMultiple');
-//const testPavoniPrice = require('./productPrices/testPavoniPrice');
 const scrapeAndComparePricesOnRequest = require('./productPrices/scrapeAndComparePricesOnRequest');
 const scrapeAndComparePricesOnSchedule = require('./productPrices/scrapeAndComparePricesOnSchedule');
 const onProductSaleCheckExecution = require('./productPrices/onProductSaleCheckExecution');
 
-//exports.testPavoniPriceMultiple = testPavoniPriceMultiple.testPavoniPriceMultiple;
-//exports.testPavoniPrice = testPavoniPrice.testPavoniPrice;
 exports.scrapeAndComparePricesOnRequest = scrapeAndComparePricesOnRequest.scrapeAndComparePricesOnRequest;
 exports.scrapeAndComparePricesOnSchedule = scrapeAndComparePricesOnSchedule.scrapeAndComparePricesOnSchedule;
 exports.onProductSaleCheckExecution = onProductSaleCheckExecution.onProductSaleCheckExecution;
