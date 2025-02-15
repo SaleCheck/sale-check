74e6acb4104933118d09b719e946eff279cb25f9
MathiasSchindlerCPH <mathingvid@gmail.com>
2025-02-15 22:31:33 +0100

    Revert "delete function for testing"
    This reverts commit b06a0c91d8e69e144114e93e8469b6a0fdf2d670.

--- functions/index.js
+++ functions/index.js
@@ -14,3 +14,14 @@ exports.scrapeAndComparePricesOnSchedule = scrapeAndComparePricesOnSchedule;
 exports.onProductSaleCheckExecution = onProductSaleCheckExecution.onProductSaleCheckExecution;
 exports.createProductToCheck = createProductToCheck.createProductToCheck;
 exports.testPuppeteer = testPuppeteer.testPuppeteer;
+
+
+
+const { onRequest } = require("firebase-functions/v2/https");
+const cors = require('cors')({ origin: true });
+
+exports.gitHubActionTestDeployOnlyFunctions = onRequest({ timeoutSeconds: 300, memory: "1GiB" }, async (req, res) => {
+    cors(req, res, async () => {
+        res.status(200).send(`Smiley day to ya☀`);
+    })
+});
