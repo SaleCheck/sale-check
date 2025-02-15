1f2eaa43df53f34c693b91078641c7993a302b48
MathiasSchindlerCPH <mathingvid@gmail.com>
2025-02-15 20:05:50 +0100

    send email notification upon completion

--- functions/productPrices/scrapeAndComparePricesOnRequest.js
+++ functions/productPrices/scrapeAndComparePricesOnRequest.js
@@ -1,6 +1,7 @@
 require("firebase-functions/logger/compat");
 const { onRequest } = require("firebase-functions/v2/https");
 const scrapeAndComparePricesAlgorithm = require("../utils/scrapeAndComparePricesAlgorithm");
+const { sendEmail } = require('../utils/emailService');
 
 exports.scrapeAndComparePricesOnRequest = onRequest({ timeoutSeconds: 300, memory: "1GiB" }, async (req, res) => {
     if (req.method !== "GET") return res.status(405).send({ success: false, error: 'Method Not Allowed. Only GET requests are allowed.' });
@@ -8,9 +9,49 @@ exports.scrapeAndComparePricesOnRequest = onRequest({ timeoutSeconds: 300, memor
     try {
         const executionResults = await scrapeAndComparePricesAlgorithm();
         res.status(200).json({ success: true, executions: executionResults });
+
+        const emailSubject = `✅SaleCheck Price Monitoring Ran Successfully`;
+        const emailBody = `
+            <p>
+                The SaleCheck price monitoring scheduler has successfully run today.
+                <br><br>
+                Please note: Products without price changes <strong>will not</strong> trigger notification emails.
+                <br><br>
+                Kind regards and smiley day to you☀️
+                <br>
+                – Team SaleChecker😻
+            </p>`;        
+        const mailOptions = {
+            from: process.env.EMAILUSER,
+            to: process.env.EMAILUSER,
+            subject: emailSubject,
+            html: emailBody
+        };
+        await sendEmail(mailOptions);
         
     } catch (error) {
         console.log('Error scraping the webpage:', error);
         res.status(500).send({ success: false, error: 'An error occurred while scraping the webpage.' });
+
+        const emailSubject = `❗ERROR: SaleCheck Scheduled Job Failed`;
+        const emailBody = `
+            <p>
+                The scheduled SaleCheck job encountered an error and did not complete successfully.
+                <br><br>
+                <strong>Error Details:</strong>  
+                <pre>${error.message}</pre>
+                <br>
+                Consider investigating this issue to ensure continued monitoring.
+                <br><br>
+                Kind regards,  
+            Team SaleChecker
+            </p>`;        
+        const mailOptions = {
+            from: process.env.EMAILUSER,
+            to: process.env.EMAILUSER,
+            subject: emailSubject,
+            html: emailBody
+        };
+        await sendEmail(mailOptions);
     }
 });
--- functions/productPrices/scrapeAndComparePricesOnSchedule.js
+++ functions/productPrices/scrapeAndComparePricesOnSchedule.js
@@ -1,6 +1,7 @@
 require("firebase-functions/logger/compat");
 const { onSchedule } = require("firebase-functions/v2/scheduler");
 const scrapeAndComparePricesAlgorithm = require("../utils/scrapeAndComparePricesAlgorithm");
+const { sendEmail } = require('../utils/emailService');
 
 exports.scrapeAndComparePricesOnSchedule = onSchedule({
         schedule: "0 7 * * 1,3,5",
@@ -20,7 +21,48 @@ exports.scrapeAndComparePricesOnSchedule = onSchedule({
         console.log("Scheduled function triggered.");
         await scrapeAndComparePricesAlgorithm();
         console.log("Scraping and comparison completed successfully.");
+        
+        const emailSubject = `✅SaleCheck Price Monitoring Ran Successfully`;
+        const emailBody = `
+            <p>
+                The SaleCheck price monitoring scheduler has successfully run today.
+                <br><br>
+                Please note: Products without price changes <strong>will not</strong> trigger notification emails.
+                <br><br>
+                Kind regards and smiley day to you☀️
+                <br>
+                – Team SaleChecker😻
+            </p>`;        
+        const mailOptions = {
+            from: process.env.EMAILUSER,
+            to: process.env.EMAILUSER,
+            subject: emailSubject,
+            html: emailBody
+        };
+        await sendEmail(mailOptions);
+
     } catch (error) {
         console.error("Error in scheduled function:", error);
+
+        const emailSubject = `❗ERROR: SaleCheck Scheduled Job Failed`;
+        const emailBody = `
+            <p>
+                The scheduled SaleCheck job encountered an error and did not complete successfully.
+                <br><br>
+                <strong>Error Details:</strong>  
+                <pre>${error.message}</pre>
+                <br>
+                Consider investigating this issue to ensure continued monitoring.
+                <br><br>
+                Kind regards,  
+            Team SaleChecker
+            </p>`;        
+        const mailOptions = {
+            from: process.env.EMAILUSER,
+            to: process.env.EMAILUSER,
+            subject: emailSubject,
+            html: emailBody
+        };
+        await sendEmail(mailOptions);
     }
 });
