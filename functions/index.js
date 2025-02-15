c0f83d76d0388c26e1f4c3d9d221191f87520f10
MathiasSchindlerCPH <mathingvid@gmail.com>
2025-02-15 20:30:08 +0100

    consolidate functions into single file with centrally defined logic for DNRY compliance

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,18 @@
 const { initializeApp } = require("firebase-admin/app");
 initializeApp();
 
-const scrapeAndComparePricesOnRequest = require('./productPrices/scrapeAndComparePricesOnRequest');
-const scrapeAndComparePricesOnSchedule = require('./productPrices/scrapeAndComparePricesOnSchedule');
+//const scrapeAndComparePricesOnRequest = require('./productPrices/scrapeAndComparePricesOnRequest');
+//const scrapeAndComparePricesOnSchedule = require('./productPrices/scrapeAndComparePricesOnSchedule');
+const {
+    scrapeAndComparePricesOnRequest,
+    scrapeAndComparePricesOnSchedule
+} = require('./productPrices/scrapeAndComparePrices');
 const onProductSaleCheckExecution = require('./productPrices/onProductSaleCheckExecution');
 const createProductToCheck = require('./firestore/createProductToCheck');
 const testPuppeteer = require('./utils/testPuppeteer');
 
-exports.scrapeAndComparePricesOnRequest = scrapeAndComparePricesOnRequest.scrapeAndComparePricesOnRequest;
-exports.scrapeAndComparePricesOnSchedule = scrapeAndComparePricesOnSchedule.scrapeAndComparePricesOnSchedule;
+exports.scrapeAndComparePricesOnRequest = scrapeAndComparePricesOnRequest;
+exports.scrapeAndComparePricesOnSchedule = scrapeAndComparePricesOnSchedule;
 exports.onProductSaleCheckExecution = onProductSaleCheckExecution.onProductSaleCheckExecution;
 exports.createProductToCheck = createProductToCheck.createProductToCheck;
 exports.testPuppeteer = testPuppeteer.testPuppeteer;
