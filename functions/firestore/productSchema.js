2f6d72dba80f5547bc150666ba8830c8231f934d
MathiasSchindlerCPH <mathingvid@gmail.com>
2025-03-02 23:06:43 +0100

    add commin firestore property definer

---
+++ functions/firestore/productSchema.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ALLOWED_FIELDS: [
+        "productName",
+        "expectedPrice",
+        "expectedPriceCurrency",
+        "url",
+        "emailNotification",
+        "cssSelector"
+    ]
+};
