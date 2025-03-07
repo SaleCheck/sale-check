0d8cb983114d1762b9cb3330b1f8e4d400c9da67
MathiasSchindlerCPH <mathingvid@gmail.com>
2025-03-07 21:03:55 +0100

    Revert "add basic auth"
    This reverts commit 8dfd67403611ba882199524ccb0c62fc57437475.

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,5 @@
-const { initializeApp: initializeAdminApp } = require("firebase-admin/app");
-initializeAdminApp();
-
-const { initializeApp } = require('firebase/app');
-const firebaseConfig = {
-    apiKey: "AIzaSyDZOYnEHUA3I1PS36teLMmKirTXpMVzFAc",
-    authDomain: "sale-check-b611b.firebaseapp.com",
-    projectId: "sale-check-b611b",
-    storageBucket: "sale-check-b611b.appspot.com",
-    messagingSenderId: "712429550284",
-    appId: "1:712429550284:web:3fdb2d4ab2e5b99448a069",
-    measurementId: "G-743W5CC4YL"
-};
-initializeApp(firebaseConfig);
-
+const { initializeApp } = require("firebase-admin/app");
+initializeApp();
 
 const {
     scrapeAndComparePricesOnRequest,
@@ -20,12 +7,10 @@ const {
 } = require('./productPrices/scrapeAndComparePrices');
 const onProductSaleCheckExecution = require('./productPrices/onProductSaleCheckExecution');
 const testPuppeteer = require('./utils/testPuppeteer');
-const createProductToCheck = require('./firestore/products/createProductToCheck');
-const getProductToCheck = require('./firestore/products/getProductToCheck');
-const updateProductToCheck = require('./firestore/products/updateProductToCheck');
-const deleteProductToCheck = require('./firestore/products/deleteProductToCheck');
-const copyUserObjectToFirestore = require('./firestore/users/copyUserObjectToFirestore');
-const createUser = require('./auth/createUser');
+const createProductToCheck = require('./firestore/createProductToCheck');
+const getProductToCheck = require('./firestore/getProductToCheck');
+const updateProductToCheck = require('./firestore/updateProductToCheck');
+const deleteProductToCheck = require('./firestore/deleteProductToCheck');
 
 exports.scrapeAndComparePricesOnRequest = scrapeAndComparePricesOnRequest;
 exports.scrapeAndComparePricesOnSchedule = scrapeAndComparePricesOnSchedule;
@@ -35,5 +20,3 @@ exports.createProductToCheck = createProductToCheck.createProductToCheck;
 exports.getProductToCheck = getProductToCheck.getProductToCheck;
 exports.updateProductToCheck = updateProductToCheck.updateProductToCheck;
 exports.deleteProductToCheck = deleteProductToCheck.deleteProductToCheck;
-exports.copyUserObjectToFirestore = copyUserObjectToFirestore.copyUserObjectToFirestore;
-exports.createUser = createUser.createUser;
