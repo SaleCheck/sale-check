6ab7558836ea9e695c9ca94c4576ae729cec3e4c
MathiasSchindlerCPH <mathingvid@gmail.com>
2025-03-15 21:03:01 +0100

    attempt 1 with many issues

--- public/script.js
+++ public/script.js
@@ -1,3 +1,44 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"; 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZOYnEHUA3I1PS36teLMmKirTXpMVzFAc",
+    authDomain: "sale-check-b611b.firebaseapp.com",
+    projectId: "sale-check-b611b",
+    storageBucket: "sale-check-b611b.appspot.com",
+    messagingSenderId: "712429550284",
+    appId: "1:712429550284:web:3fdb2d4ab2e5b99448a069",
+    measurementId: "G-743W5CC4YL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById('auth-login').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    // Show the loading overlay and spinner
+    document.getElementById('loading-overlay').style.display = 'block';
+    document.getElementById('loading-spinner').style.display = 'block';
+
+    const userName = document.getElementById('user').value;
+    const pwd = document.getElementById('pwd').value;
+
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, userName, pwd);
+        const user = userCredential.user;
+        console.log('Success:', user);
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('message').innerHTML = `<p style="color: red;">Error: ${error.message}</p>`;
+    } finally {
+        // Hide the loading overlay and spinner
+        document.getElementById('loading-overlay').style.display = 'none';
+        document.getElementById('loading-spinner').style.display = 'none';
+    }
+});
+
 document.getElementById('crud-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
