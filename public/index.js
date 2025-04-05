a8ba10fb6c77bd7fac491e2b04c52c412799ee1e
MathiasSchindlerCPH <mathingvid@gmail.com>
2025-04-05 18:26:21 +0200

    prepare auth

--- public/index.js
+++ public/index.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZOYnEHUA3I1PS36teLMmKirTXpMVzFAc",
+    authDomain: "sale-check-b611b.firebaseapp.com",
+    projectId: "sale-check-b611b",
+    storageBucket: "sale-check-b611b.appspot.com",
+    messagingSenderId: "712429550284",
+    appId: "1:712429550284:web:3fdb2d4ab2e5b99448a069",
+    measurementId: "G-743W5CC4YL"
+};
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+console.log("Firebase Auth instance:", auth);
+
 document.getElementById('crud-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
