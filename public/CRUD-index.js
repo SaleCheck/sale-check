46406b04e4492a447927da4a38c6b604e9c66882
MathiasSchindlerCPH <mathingvid@gmail.com>
2025-04-06 13:11:35 +0200

    move js code to crud-index.js

--- public/CRUD-index.js
+++ public/CRUD-index.js
@@ -1,3 +1,38 @@
+// Firebase configuration and initialization
+const firebaseConfig = {
+    apiKey: "AIzaSyDZOYnEHUA3I1PS36teLMmKirTXpMVzFAc",
+    authDomain: "sale-check-b611b.firebaseapp.com",
+    projectId: "sale-check-b611b",
+    storageBucket: "sale-check-b611b.appspot.com",
+    messagingSenderId: "712429550284",
+    appId: "1:712429550284:web:3fdb2d4ab2e5b99448a069",
+    measurementId: "G-743W5CC4YL"
+};
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in
+        const email = user.email;
+        const photoURL = user.photoURL || 'https://via.placeholder.com/50'; // Default avatar if none is provided
+
+        // Update the header
+        const userHeader = document.getElementById('user-header');
+        const welcomeMessage = document.getElementById('welcome-message');
+        const userAvatar = document.getElementById('user-avatar');
+
+        welcomeMessage.textContent = `Welcome, ${email}`;
+        userAvatar.src = photoURL;
+
+        userHeader.style.display = 'block';
+    } else {
+        // User is not signed in, redirect to login page
+        window.location.href = "index.html";
+    }
+});
+
+
 document.getElementById('crud-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -58,3 +93,4 @@ document.getElementById('clear-button').addEventListener('click', function () {
 
     document.getElementById('message').innerHTML = '';
 });
+
