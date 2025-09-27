2bee678d4b5e5d3b96faaf023a56ac4569ec0c9a
Mathias Schindler <mathingvid@gmail.com>
2025-09-27 16:17:36 +0200

    feat: build simple landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import favicon from '/favicon.png'
 import './App.css'
+import favicon from '/favicon.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://sale-check-b611b.web.app/" target="_blank">
-          <img src={favicon} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div style={{ textAlign: 'center' }}>
+      {/* Favicon Image */}
+      <img
+        src={favicon}
+        alt="SaleCheck Favicon"
+        style={{ height: '250px', display: 'block', margin: '0 auto' }}
+      />
+
+      {/* Main Header */}
+      <h1>Welcome to SaleCheck</h1>
+
+      {/* Introduction Text */}
+      <p className="intro-text">
+        We provide you with the service that makes tracking price drops a breeze. Enter the product URL and your target
+        price, and our system will keep a close eye on any price changes for you. We’ll notify you the moment your item is
+        on sale, so you can seize the deal without having to constantly check.
+        <br /><br />
+        Let us do the hard work of monitoring prices, so you can enjoy your savings effortlessly!
       </p>
-    </>
+    </div>
   )
 }
 
